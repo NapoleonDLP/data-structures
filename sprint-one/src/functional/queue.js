f0299ee2b276112fa242949512913c28f515ceee
NapoleonDLP <napoleon.dlp@gmail.com>
2019-05-29 15:59:49 -0700

    Completed functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,7 +2,7 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  someInstance.Storage = {};
+  someInstance.storage = {};
 
   // Add two properties, front & back
   someInstance.front = 0;
@@ -12,19 +12,25 @@ var Queue = function() {
 
   someInstance.enqueue = function(value) {
     // add value to storage at back of queue
-
+    someInstance.storage[someInstance.back] = value;
     // increment back
+    someInstance.back++;
   };
 
   someInstance.dequeue = function() {
     // store current value
+    var value = someInstance.storage[someInstance.front];
     // delete storage[front]
+    delete someInstance.storage[someInstance.front];
     // increment front
+    someInstance.back > someInstance.front && someInstance.front++;
     // return stored value
+    return value;
   };
 
   someInstance.size = function() {
     //return back - front
+    return someInstance.back - someInstance.front;
   };
 
   return someInstance;
