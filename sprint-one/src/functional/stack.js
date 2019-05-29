7fe3c469151e89c8be5851d4462022e8e9baf168
NapoleonDLP <napoleon.dlp@gmail.com>
2019-05-29 14:54:32 -0700

    Completed pseudo code, first attempt at push method

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,17 +2,50 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
-
+  someInstance.storage = {};
+  //add variable for storage track of count
+  someInstance.count = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    //add to storage obj using count:value
+    someInstance.storage[someInstance.count] = value;
+    //  increment count
+    someInstance.count++;
+    /*
+I : string
+O : no output just side effect
+C : n/a
+E : n/a
+*/
   };
 
   someInstance.pop = function() {
+    //  Remove and return the string on the top of the stack
+    /*
+    I : N/A
+    O : string from top of stack
+    C : n/a
+    E : n/a
+    */
+    //  store value in variable
+    var value = someInstance.storage[someInstance.count];
+    //  delete key:value from storage
+    //  decrement count
+    //  return value at the current count in storage
   };
 
   someInstance.size = function() {
+    //return count
   };
+
+  //  add storage as a property of someInstance before returning
 
   return someInstance;
 };
+
+/*
+I
+O
+C
+E
+*/
