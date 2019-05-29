5c43c2f351295c5b0b9bf7bd0e2434a994453f65
NapoleonDLP <napoleon.dlp@gmail.com>
2019-05-29 15:54:10 -0700

    Attempt to complete  enqueue and dequeue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,29 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  someInstance.Storage = {};
+
+  // Add two properties, front & back
+  someInstance.front = 0;
+  someInstance.back = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // add value to storage at back of queue
+
+    // increment back
   };
 
   someInstance.dequeue = function() {
+    // store current value
+    // delete storage[front]
+    // increment front
+    // return stored value
   };
 
   someInstance.size = function() {
+    //return back - front
   };
 
   return someInstance;
