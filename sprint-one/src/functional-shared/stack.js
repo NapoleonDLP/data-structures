c5323b788ea45dd0e2eb7d7499195aab564f6bf2
NapoleonDLP <napoleon.dlp@gmail.com>
2019-05-29 16:29:15 -0700

    Completed pseudo coding for functional-shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //declare var someinstance set to {}
+  //declare storage property in someinstance set to{}
+
+  //declare var set to size of stack
+
+  //extend instance with stackmethods
+
+  //return instance
+
 };
 
 var stackMethods = {};
 
+// Add push to stackMethods
+// Add pop to stackMethods
+// Add size to stackMethods
 
