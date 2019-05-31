fd4b1711cc016441f9ab62176de4c6ffb2acc5fb
Garrett Watson <garrettbauer@gmail.com>
2019-06-01 03:23:34 +0900

    Completed Queue pseudoclass

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,10 +1,10 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.
 
-  // this.storage = {}
-  //this.front = 0;
-  //this.back = 0;
+  this.storage = {};
+  this.front = 0;
+  this.back = 0;
 };
 
 // add enqueue
