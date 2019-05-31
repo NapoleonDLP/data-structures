e12e26e3bea94eaea785c8bc581c750929468687
Garrett Watson <garrettbauer@gmail.com>
2019-06-01 03:22:38 +0900

    Completed pseudocode

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,19 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // this.storage = {}
+  //this.front = 0;
+  //this.back = 0;
 };
 
+// add enqueue
+//index = back
+
+//add dequeue
+//index = front
+// back > front
+
+//add size
+//back - front
 
