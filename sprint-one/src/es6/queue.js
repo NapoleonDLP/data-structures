2b0bc59c176f917c6e174736210bb2e5aca434f8
Garrett Watson <garrettbauer@gmail.com>
2019-06-01 03:54:27 +0900

    Completed pseudocode

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,14 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    // this.storage = {}
+    // this.front
+    // this.back
   }
 
+  // add enqueue
+
+  // add dequeue
+
+  // add size
 }
