ee3c9fdbb6670a2d6e65d40830cabc8faec7a83f
Garrett Watson <garrettbauer@gmail.com>
2019-06-01 03:45:54 +0900

    Completed constructor and push method

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,11 +2,15 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-    //this.storage
-    //this.count
+    this.storage = {};
+    this.count = 0;
   }
 
   // add push method
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
 
   // add pop
 
