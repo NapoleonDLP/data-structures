19f2c85d186d1db1e2a8933bce7ed52d81882326
Garrett Watson <garrettbauer@gmail.com>
2019-06-01 03:55:28 +0900

    Completed Queue class constructor

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,12 +2,13 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-    // this.storage = {}
-    // this.front
-    // this.back
+    this.storage = {};
+    this.front = 0;
+    this.back = 0;
   }
 
   // add enqueue
+
 
   // add dequeue
 
