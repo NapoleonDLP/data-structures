c57f2a22bfa41d91ba209e73464ace981cfae325
Garrett Watson <garrettbauer@gmail.com>
2019-06-01 03:10:08 +0900

    Supply finished pseudocode

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // declare this.count
+  this.count = 0;
+  // declare this.storage
+  this.storage = {};
 };
+
+// add push to Stack.prototype
+// index = this.count
+
+// add pop to Stack.prototype
+// index = count -1
+//count > 0
+
+// add size to Stack.prototype
 
 
