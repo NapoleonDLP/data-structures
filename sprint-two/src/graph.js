811d0632cda4f30a15b44ed99352f0f086680794
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-01 14:03:21 -0700

    Completed node addNode, contains, remodeNode methods

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,18 +2,28 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.storage = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.storage[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.storage.hasOwnProperty(node);
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  // iterate over each edge
+  this.storage[node].forEach(neighbor => {
+    this.storage[neighbor].filter(x => x !== node);
+  });
+  // go to that edge
+  // find index of nodeToDelete
+  delete this.storage[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
@@ -37,3 +47,9 @@ Graph.prototype.forEachNode = function(cb) {
  */
 
 
+// storage = {
+//   node1: [node2, node5]
+//   node2: [node1,]
+//   ...
+//   node5: [node1,]
+// }
