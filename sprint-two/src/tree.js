3488436f15d031cec7c3b9a1a8106fdff1f7f8b2
Garrett Watson <garrettbauer@gmail.com>
2019-06-02 03:09:41 +0900

    Completed Tree constructor, added addChild, contains methods

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,34 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  newTree.children = []; // fix me
 
-  // your code here
-  newTree.children = null;  // fix me
-
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var node = this;
+  // declare found
+  var found = false;
+  // declare helper function (node)
+  var findTarget = function(node) {
+    if (node.value === target) {
+      found = true;
+      return;
+    }
+    //iterate through children
+    node.children.forEach( child => {
+      findTarget(child);
+    });
+  };
+  return found;
 };
 
 
