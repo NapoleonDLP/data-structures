e910a9e81f12d654ebb35de5d3f3752df058a35d
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-01 15:34:29 -0700

    Completed pseudo for set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,21 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // add item to set
 };
 
 setPrototype.contains = function(item) {
+  //check if item is found in set
 };
 
 setPrototype.remove = function(item) {
+  //delete item from set
 };
 
 /*
