58d7679ad06de74a52e7bab8ffa3cd530bf054ca
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-01 16:09:44 -0700

    Completed pseudo for hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,19 +2,23 @@
 
 var HashTable = function() {
   this._limit = 8;
+  // creates the array/hastable
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //set value at key on array
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //get value at key on array
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //set value to undefined at key
 };
 
 
