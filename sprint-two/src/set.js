5e27fcfc3e46b1edb4bf44922ff7de5380286f94
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-01 15:44:51 -0700

    Completed time complexities

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -6,17 +6,17 @@ var Set = function() {
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function(item) { //O(1) Constant
   // add item to set
   this._storage[item] = item;
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function(item) { //O(n) Linear
   //check if item is found in set
   return this._storage.hasOwnProperty(item);
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function(item) { //O(1) Constant
   //delete item from set
   delete this._storage[item];
 };
