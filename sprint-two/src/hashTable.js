8925dbb48611c9f0cc1936cb362b6749c832a0bc
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-01 16:57:07 -0700

    Completed collision handling pseudo code

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,18 +8,33 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //account for two possible situations
+  //does the bucket exist
+  //  tore bucket to var
+  // push key value touple to bucket
+  // set bucket at index
+  //else
+  // set an array of k v touple at index
   this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  //get value at key on array
+  //get and store value at index
+  //iterate through bucket
+  // checking for key
+  // return value
   return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  //set value to undefined at key
+  //get and store value at index
+  //iterate through bucket
+  // checking for key
+  // filter through bucket
+  // set index to filtered bucket
+  this._storage.set(index, undefined);
 };
 
 
