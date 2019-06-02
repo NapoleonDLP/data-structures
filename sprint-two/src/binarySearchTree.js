2ff842816ff3f592618216dc91538bda159d0a67
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-02 11:54:44 -0700

    Completed pseudo for BinarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,16 @@
 var BinarySearchTree = function(value) {
+  //instantiate obj (prototypal)
+  //store value
+  //declare left node = null
+  //declare right node = null
+  //return object
 };
 
+//add insert to methods
+
+//add contains
+
+//add depthfirstlog
 
 /*
  * Complexity: What is the time complexity of the above functions?
