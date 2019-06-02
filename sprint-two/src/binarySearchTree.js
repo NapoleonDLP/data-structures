57a30b0a4e096463fd55a97e857c51ea8d3d65d2
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-02 12:57:37 -0700

    Completed insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,13 +1,21 @@
 var BinarySearchTree = function(value) {
   //instantiate obj (prototypal)
   var obj = Object.create(BinarySearchTree.prototype);
-  this.value = value;
-  this.left = null;
-  this.right = null;
+  obj.value = value;
+  obj.left = null;
+  obj.right = null;
   return obj;
 };
 
-//add insert to methods
+BinarySearchTree.prototype.insert = function(inputValue) {
+  var direction = inputValue < this.value ? 'left' : 'right';
+  if (this[direction] === null) {
+    this[direction] = BinarySearchTree(inputValue);
+    return;
+  }
+  this[direction].insert(inputValue);
+};
+
 
 //add contains
 
@@ -15,4 +23,23 @@ var BinarySearchTree = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ */
+
+
+/*
+  var myTree = BinarySearchTree(25);
+  myTree.insert(5);
+
+
+  var tree = {
+  value: 8
+    left : {
+      value: 3,
+      left : {
+        value 1
+      }
+      right
+    }
+    right:
+  }
  */
